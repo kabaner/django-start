6513f0b50d04ce2c9dcc8da6ed9c38b3451c8ac3
xianhaixiong <xianhaixiong@163.com>
2016-06-20 00:50:34 -0400

    post get xianhaixiong

--- first/first/urls.py
+++ first/first/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
 from first.view import hello
 from first.testdb import testdb
+from first import search
+from first import search2
 
 urlpatterns = [
     url('^hello/$', hello),
     url('^testdb/$', testdb),
+    url('^search-form/$', search.search_form),
+    url('^search/$', search.search),
+    url('^search-post/$', search2.search_post),
 ]
